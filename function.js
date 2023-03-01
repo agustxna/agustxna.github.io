1f5d8ec2cf96d19c96b7dfa9344118f8e4b3dec2
agustxna <126633858+agustxna@users.noreply.github.com>
2023-02-28 18:11:33 -0800

    v1

---
+++ function.js
@@ -0,0 +1,11 @@
+const btnSwitch = document.getElementById('switch');
+btnSwitch.addEventListener('click',()=>{
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
+
+
+
+
+
+
